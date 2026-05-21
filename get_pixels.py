c1ae7e2bf7f7cf5bb9fbd52227e8af410619312f
Sajjad <Sajjad.120nkst@gmail.com>
2026-05-21 12:35:26 +0330

    feat: implement core image processing logic
    - GetPixel class for reading image pixels
    - Get image dimensions and pixel colors
    - Convert RGB values to hex color format

---
+++ get_pixels.py
@@ -0,0 +1,28 @@
+from PIL import Image
+
+class GetPixel():
+    def __init__(self, name="example.jpg"):
+        self.name = name
+    
+    def get_size(self):
+        self.image = Image.open(self.name)
+        self.width, self.height = self.image.size
+        print(f"image'size: ({self.width}, {self.height})")
+
+    def get_pixel(self):
+        self.pixels = []
+        for x in range(self.width):
+            for y in range(self.height):
+                R, G, B = self.image.getpixel((x, y))
+                color = f"#{R:02x}{G:02x}{B:02x}".upper()
+                self.pixels.append(color)
+        print("Image successfully analizied")
+    
+    def close_image(self):
+        self.image.close()
+
+if __name__ == "__main__":
+    new_image = GetPixel()
+    new_image.get_size()
+    new_image.get_pixel()
+    new_image.close_image()
